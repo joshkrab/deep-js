b0c44c82acef6ca7bcbe2a38cbd96361579e6b98
Ihor Neizvestnyi <neizvestnyi.igor@gmail.com>
2022-09-09 22:54:59 +0300

    gfgf

--- 07 Classes/index.js
+++ 07 Classes/index.js
@@ -30,9 +30,10 @@ class Cat extends Animal {
 
   constructor(obj) {
     super(obj); // Визиває конструктор класа батька Animal, інакше помилка
-    this.color = obj.color
+    this.color = obj.color // Якби додаємо до конструктора батьківського класу
   }
 
+  // Додали метот поточному класу, інакче буде визиватись цей метод у батьківського
   voice() {
     super.voice(); // Визове метод батьківського класу
     console.log('I am cat');
@@ -60,4 +61,77 @@ console.log(cat.ageInfo); // 35
 cat.ageInfo = 2; // Задали сетером нове значення
 console.log(cat.ageInfo); // 14
 
-// 11 хвилина
+// Приклад 2 ------------------------------------------------------------------------------------------------
+
+class Component {
+  constructor(selector) {
+    // .$el - просто ім'я властивості для дом елемента:
+    this.$el = document.querySelector(selector)
+  }
+
+  hide() {
+    this.$el.style.display = 'none'
+  }
+
+  show() {
+    this.$el.style.display = 'block'
+  }
+}
+
+class Box extends Component {
+  constructor(options) {
+    super(options.selector)
+
+    this.$el.style.width = this.$el.style.height = options.size + 'px'
+    this.$el.style.background = options.color
+  }
+}
+
+const box1 = new Box({
+  selector: '#box1',
+  size: 100,
+  color: 'red'
+})
+
+const box2 = new Box({
+  selector: '#box2',
+  size: 120,
+  color: 'blue'
+})
+
+class Circle extends Box {
+  constructor(options) {
+    super(options)
+
+    this.$el.style.borderRadius = '50%'
+  }
+}
+
+const c = new Circle({
+  selector: '#circle',
+  size: 90,
+  color: 'green'
+})
+
+// Інкапсуляція - упаковка, приховання даних від прямого доступу зовні. 
+// Наприклад ми створюємо локальну змінну всередині функції конструктора і можемо її змінити тільки, 
+// якщо аргумент пришедший в сетер відповідає нашим умовам, 
+// тобто ми контролюємо зміну локальної(прихованої) змінної
+
+function User (name, age) {
+    this.name = name;
+    var _age = age; // локальна змінна
+    this.displayInfo = function(){
+        document.write("Имя: " + this.name + "; возраст: " + _age + "<br>");
+    };
+    this.getAge = function() {
+        return _age;
+    }
+    this.setAge = function(age) {
+        if(typeof age === "number" && age >0 && age<110){
+            _age = age; // змінюємо локальну змінну при виконанні умов
+        } else {
+            console.log("Недопустимое значение");
+        }
+    }
+}
