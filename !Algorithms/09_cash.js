47bb9d35f711c3f710baa93ef713e98e5308dedf
Ihor Neizvestnyi <neizvestnyi.igor@gmail.com>
2022-12-14 19:42:57 +0200

    cash

--- !Algorithms/09_cash.js
+++ !Algorithms/09_cash.js
@@ -1,1 +1,33 @@
-// 45:18
+function cashFunction(fn) {
+	const cash = {};
+	return function (n) {
+		if (cash[n]) {
+			console.log('From cash: ',cash[n]);
+			return cash[n];
+		}
+		cash[n] = fn(n);
+		console.log('From func ',cash[n]);
+		return cash[n];
+	}
+};
+
+function factorial(n) {
+	let result = 1;
+	while (n !== 1) {
+		result *= n;
+		n--;
+	}
+	return result;
+}
+// console.log(factorial(5));
+
+const cashFactorial = cashFunction(factorial);
+
+cashFactorial(5);
+cashFactorial(4);
+cashFactorial(3);
+cashFactorial(3);
+cashFactorial(4);
+cashFactorial(5);
+cashFactorial(6);
+cashFactorial(6);
