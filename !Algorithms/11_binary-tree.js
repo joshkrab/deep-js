987299e6256ce04b1a52522873fa47f0c7b4bbdc
Ihor Neizvestnyi <neizvestnyi.igor@gmail.com>
2022-12-16 21:13:38 +0200

    gohj

--- !Algorithms/11_binary-tree.js
+++ !Algorithms/11_binary-tree.js
@@ -1,1 +1,64 @@
+/* eslint-disable max-classes-per-file */
 // 53:33
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    // Record two references for child trees:
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  add(value) {
+    if (!this.root) {
+      this.root = new TreeNode(value);
+    } else {
+      let node = this.root;
+      const newNode = new TreeNode(value);
+
+      while (node) {
+        if (value > node.value) {
+          if (!node.right) {
+            break;
+          }
+          node = node.right;
+        } else {
+          if (!node.left) {
+            break;
+          }
+          node = node.left;
+        }
+      }
+
+      if (value > node.value) {
+        node.right = newNode;
+      } else {
+        node.left = newNode;
+      }
+    }
+  }
+
+  // eslint-disable-next-line consistent-return
+  print(root = this.root) {
+    if (!root) {
+      return true;
+    }
+    // eslint-disable-next-line no-console
+    console.log(root.value);
+    this.print(root.left);
+    this.print(root.right);
+  }
+}
+
+const tree = new BinaryTree();
+tree.add(5);
+tree.add(2);
+tree.add(6);
+tree.add(2);
+tree.add(1);
+tree.print();
--- index.js
+++ index.js
@@ -10,13 +10,13 @@ function outerFunc(a) {
 
 var func = outerFunc(a);
 a = 30;
-func();
+// func();
 
 function arrayRange(a, b) {
 	return new Array(b).fill().map(i => a++);
 }
-console.log(arrayRange(1,4));
-console.log(arrayRange(-6,4));
+// console.log(arrayRange(1,4));
+// console.log(arrayRange(-6,4));
 
 function greetingMsg() {
 	console.log(customerName);
@@ -26,3 +26,48 @@ function greetCustomer() {
 	greetingMsg()
 };
 //greetCustomer(); // error
+
+function findEvenIndex(arr)
+{
+  for(let i=0; i<arr.length; i++){
+    let left = 0;
+    let right = 0;
+    
+    for (let j=0; j<arr.length; j++){
+      if(j<i){ left += arr[j]};
+      if(j>i){ right += arr[j]};
+		}
+		console.log(left, right);
+    if (left === right){
+      return i;
+    }
+  }
+  return -1;
+}
+// console.log(findEvenIndex([1,2,3,4,3,2,1]));
+function greet(language) {
+	const dataBase = {
+    english: 'Welcome',
+czech: 'Vitejte',
+danish: 'Velkomst',
+dutch: 'Welkom',
+estonian: 'Tere tulemast',
+finnish: 'Tervetuloa',
+flemish: 'Welgekomen',
+french: 'Bienvenue',
+german: 'Willkommen',
+irish: 'Failte',
+italian: 'Benvenuto',
+latvian: 'Gaidits',
+lithuanian: 'Laukiamas',
+polish: 'Witamy',
+spanish: 'Bienvenido',
+swedish: 'Valkommen',
+welsh: 'Croeso'
+  }
+  if (dataBase[language]){
+    return dataBase[language];
+  }
+  return 'IP_ADDRESS_NOT_FOUND';
+}
+console.log(greet('english'));
