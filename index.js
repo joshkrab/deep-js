c9ba36adc7267419be860ba525915d56a9230d8b
Ihor Neizvestnyi <neizvestnyi.igor@gmail.com>
2022-12-19 14:05:00 +0200

    08

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var a = 10;
 
 function outerFunc(a) {
-	function innerFunc() {
-		var b = 20;
-		console.log(a + b);
-	}
-	return innerFunc;
+  function innerFunc() {
+    var b = 20;
+    console.log(a + b);
+  }
+  return innerFunc;
 }
 
 var func = outerFunc(a);
@@ -13,32 +13,31 @@ a = 30;
 // func();
 
 function arrayRange(a, b) {
-	return new Array(b).fill().map(i => a++);
+  return new Array(b).fill().map(i => a++);
 }
 // console.log(arrayRange(1,4));
 // console.log(arrayRange(-6,4));
 
 function greetingMsg() {
-	console.log(customerName);
+  console.log(customerName);
 };
 function greetCustomer() {
-	var customerName = 'Andrew';
-	greetingMsg()
+  var customerName = 'Andrew';
+  greetingMsg()
 };
 //greetCustomer(); // error
 
-function findEvenIndex(arr)
-{
-  for(let i=0; i<arr.length; i++){
+function findEvenIndex(arr) {
+  for (let i = 0; i < arr.length; i++) {
     let left = 0;
     let right = 0;
-    
-    for (let j=0; j<arr.length; j++){
-      if(j<i){ left += arr[j]};
-      if(j>i){ right += arr[j]};
-		}
-		console.log(left, right);
-    if (left === right){
+
+    for (let j = 0; j < arr.length; j++) {
+      if (j < i) { left += arr[j] };
+      if (j > i) { right += arr[j] };
+    }
+    console.log(left, right);
+    if (left === right) {
       return i;
     }
   }
@@ -46,28 +45,43 @@ function findEvenIndex(arr)
 }
 // console.log(findEvenIndex([1,2,3,4,3,2,1]));
 function greet(language) {
-	const dataBase = {
+  const dataBase = {
     english: 'Welcome',
-czech: 'Vitejte',
-danish: 'Velkomst',
-dutch: 'Welkom',
-estonian: 'Tere tulemast',
-finnish: 'Tervetuloa',
-flemish: 'Welgekomen',
-french: 'Bienvenue',
-german: 'Willkommen',
-irish: 'Failte',
-italian: 'Benvenuto',
-latvian: 'Gaidits',
-lithuanian: 'Laukiamas',
-polish: 'Witamy',
-spanish: 'Bienvenido',
-swedish: 'Valkommen',
-welsh: 'Croeso'
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
   }
-  if (dataBase[language]){
+  if (dataBase[language]) {
     return dataBase[language];
   }
   return 'IP_ADDRESS_NOT_FOUND';
 }
 console.log(greet('english'));
+
+function sumArray(n) {
+  let result = 0;
+  if (n != null && n.length > 3) {
+    let arr = n.sort((a, b) => a - b);
+    console.log(n);
+    arr.forEach((i, index, array) => {
+      if (index !== 0 && index !== array.length - 1) {
+        result += i;
+      }
+    });
+  }
+  return result;
+}
+console.log(sumArray([0, 1, 6, 10, 10]));
