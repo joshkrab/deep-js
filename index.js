87050ded1542c7741e7f5b89e0dbfaf870814cd9
Ihor Neizvestnyi <neizvestnyi.igor@gmail.com>
2022-12-20 21:00:21 +0200

    10

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var a = 10;
 
-function outerFunc(a) {
-  function innerFunc() {
+function outerFunc (a) {
+  function innerFunc () {
     var b = 20;
     console.log(a + b);
   }
@@ -12,22 +12,22 @@ var func = outerFunc(a);
 a = 30;
 // func();
 
-function arrayRange(a, b) {
+function arrayRange (a, b) {
   return new Array(b).fill().map(i => a++);
 }
 // console.log(arrayRange(1,4));
 // console.log(arrayRange(-6,4));
 
-function greetingMsg() {
+function greetingMsg () {
   console.log(customerName);
 };
-function greetCustomer() {
+function greetCustomer () {
   var customerName = 'Andrew';
   greetingMsg()
 };
 //greetCustomer(); // error
 
-function findEvenIndex(arr) {
+function findEvenIndex (arr) {
   for (let i = 0; i < arr.length; i++) {
     let left = 0;
     let right = 0;
@@ -44,7 +44,7 @@ function findEvenIndex(arr) {
   return -1;
 }
 // console.log(findEvenIndex([1,2,3,4,3,2,1]));
-function greet(language) {
+function greet (language) {
   const dataBase = {
     english: 'Welcome',
     czech: 'Vitejte',
@@ -71,7 +71,7 @@ function greet(language) {
 }
 console.log(greet('english'));
 
-function sumArray(n) {
+function sumArray (n) {
   let result = 0;
   if (n != null && n.length > 3) {
     let arr = n.sort((a, b) => a - b);
@@ -85,3 +85,16 @@ function sumArray(n) {
   return result;
 }
 console.log(sumArray([0, 1, 6, 10, 10]));
+
+// Хешування - кожну літеру (крім цифри або сімвола) перенести на 13 сімволів в алфавіті:
+function rot13 (message) {
+  const input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  const output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+
+  return message.split('').map(i => {
+    if (input.indexOf(i) >= 0) {
+      return output[input.indexOf(i)];
+    }
+    return i;
+  }).join('');
+}
